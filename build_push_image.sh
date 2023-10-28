e188770b7ccff84594dc55a27f20503d6d2ed311
LandewankFF <landewankfahrezaf@gmail.com>
2023-10-28 15:46:31 +0700

    change position

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-#image name dan image tag versi1
-image_name="item-app"
-image_tag="v1"
 
 #Membuat Docker image dari Dockerfile
 docker build -t item-app:v1 .
 
-#Set environment variable for Docker Hub password
-export PASSWORD_DOCKER_HUB=webprogramming
-#Login to Docker Hub using the environment variable
-echo $PASSWORD_DOCKER_HUB | docker login -u dicodinglff --password-stdin
-
+#menampilkan daftar images
+docker images
 
 # Mengubah nama image sesuai format Docker Hub
 docker tag item-app:v1 dicodinglff/item-app:v1
 
+#Login to Docker Hub using the environment variable
+echo $PASSWORD_DOCKER_HUB | docker login -u dicodinglff --password-stdin
+
+
+
 # Mengunggah image ke Docker Hub
 docker push dicodinglff/item-app:v1
-
-#menampilkan daftar images
-docker images
