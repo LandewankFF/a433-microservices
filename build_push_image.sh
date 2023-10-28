2989c56ba2262535674d4ec02a6f876ad5f16b03
LandewankFF <landewankfahrezaf@gmail.com>
2023-10-28 08:48:03 +0700

    revisi3

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #image name dan image tag versi1
-image_name="dicodinglff/item-app"
+image_name=item-app
 image_tag="v1"
+
+# Membuat Docker image dari Dockerfile
+docker build -t item-app:v1 .
+
 # Restart policy: always
 restart: always
 
-#Login terlebih dahulu di docker desktop 
-docker build -t "dicodinglffn/item-app:v1" .
-#upload kontainer ke docker hub
-docker push "dicodinglff/item-app:v1"
+# Mengubah nama image sesuai format Docker Hub
+docker tag item-app:v1 dicodinglff/proyek-pertama:v1
 
+# Mengunggah image ke Docker Hub
+docker push dicodinglff/proyek-pertama:v1
+
+#menampilkan daftar images
 docker images
