b20c36d4449265d6ae7eb0c5fb9f1eccf2623d52
LandewankFF <landewankfahrezaf@gmail.com>
2023-11-01 12:48:42 +0700

    add tag

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -3,6 +3,10 @@
 #Membuat Docker image dari Dockerfile
 docker build -t karsajobs:latest .
 
+# Mengubah nama image sesuai format Docker Hub
+docker tag karsajobs:latest dicodinglff/karsajobs:latest
+
+
 #Login to Docker Hub using the environment variable
 echo $PASSWORD_DOCKER_HUB | docker login -u dicodinglff --password-stdin
 
