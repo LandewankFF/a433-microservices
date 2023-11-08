eedaa7fcc0bc3a9afbe47e320befda36033ced17
LandewankFF <landewankfahrezaf@gmail.com>
2023-11-08 12:47:28 +0700

    new

--- build_push_image.sh
+++ build_push_image.sh
@@ -12,7 +12,5 @@ docker tag item-app:v1 dicodinglff/item-app:v1
 #Login to Docker Hub using the environment variable
 echo $PASSWORD_DOCKER_HUB | docker login -u dicodinglff --password-stdin
 
-
-
 # Mengunggah image ke Docker Hub
 docker push dicodinglff/item-app:v1
