5810414af7369aa4e4052a0c37e83faba641d1a6
LandewankFF <landewankfahrezaf@gmail.com>
2023-11-11 09:12:39 +0700

    change dockerfile and build image

--- build_push_order_service.sh
+++ build_push_order_service.sh
@@ -9,8 +9,8 @@ docker images
 # Mengubah nama image sesuai format Docker Hub
 docker tag order-service:v1 dicodinglff/order-service:v1
 
-# Login to Docker Hub using the environment variable
-echo $PASSWORD_DOCKER_HUB | docker login -u dicodinglff --password-stdin
+# jika belum login docker
+# echo $PASSWORD_DOCKER_HUB | docker login -u dicodinglff --password-stdin
 
 # Mengunggah image ke Docker Hub
 docker push dicodinglff/order-service:v1
